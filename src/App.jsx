c45ad3aca857ac31dc992e7dda83e69dffdce9ec
johnkali <jdkali96@gmail.com>
2025-11-13 13:59:16 +0300

    Add homepage assets and search component with search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react'
+import Search from './Components/Search.jsx';
 
 const App = () =>{
+    const [searchTerm , setSearchTerm] = useState('');
   return(
      <main>
         <div className="pattern"/>
 
         <div className="wrapper">
             <header>
+                <img src="./hero.png" alt="hero banner"/>
               <h1> Find <span className="text-gradient">Movies</span> You'll Enjoy! Without the Hassle</h1>
             </header>
-            <p>Search</p>
+           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+           <h1 className="text-white">{searchTerm}</h1>
         </div>
      </main>
     );
